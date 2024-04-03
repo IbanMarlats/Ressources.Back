77c9d286e5ccb74677457b0bfe2b29c97858d9d6
IbanMARLATS <iban.marlats@gmail.com>
2024-04-03 21:36:15 +0200

    nv controllers

---
+++ Ressources.Back.Data/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ressources.Back.Data.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Login { get; set; } = "";
+        public string Mdp { get; set; } = "";
+
+        public int Age { get; set; }
+        public string SituationFamiliale { get; set; } = "";
+
+        public string CSP { get; set; } = "";
+        public string Loisir { get; set; } = "";
+        public string Autre {  get; set; } = "";
+        public int IdTypeUser { get; set; } = 1;
+        //public virtual UserModel TypeUser { get; set; }
+    }
+}
