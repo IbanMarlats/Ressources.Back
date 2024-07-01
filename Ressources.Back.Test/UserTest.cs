54bda0112bd288f3c1efe088c16e1f95d8260d6c
Allastaria <p.mattiuzzo32@yahoo.fr>
2024-07-01 16:28:14 +0200

    correctifs

--- Ressources.Back.Test/UserTest.cs
+++ Ressources.Back.Test/UserTest.cs
@@ -23,7 +23,7 @@ namespace Ressources.Back.Api.Tests
         [Test]
         public void Post()
         {
-            var newUser = new UserModel { Id = 3, Login = "newuser", Mdp = "password", Activate = 1, Age = 25, SituationFamiliale = "Célibataire", CSP = "Étudiant", Loisir = "Musique", Autre = "Rien", IdTypeUser = 1, IdStatus = 1 };
+            var newUser = new UserModel { Id = 3, Login = "newuser", Mdp = "password", Activate = 1, Age = "25", SituationFamiliale = "Célibataire", CSP = "Étudiant", Loisir = "Musique", Autre = "Rien", IdTypeUser = 1, IdStatus = 1 };
             _mockUserRepository.Setup(repo => repo.Create(newUser)).Returns(newUser);
 
             var result = _controller.Post(newUser);
@@ -37,9 +37,9 @@ namespace Ressources.Back.Api.Tests
         public void Put()
         {
             var userId = 1;
-            var updatedUser = new UserModel { Id = userId, Login = "updateduser", Mdp = "newpassword", Activate = 1, Age = 30, SituationFamiliale = "Célibataire", CSP = "Salarié", Loisir = "Football", Autre = "Autre", IdTypeUser = 1, IdStatus = 1 };
+            var updatedUser = new UserModel { Id = userId, Login = "updateduser", Mdp = "newpassword", Activate = 1, Age = "30", SituationFamiliale = "Célibataire", CSP = "Salarié", Loisir = "Football", Autre = "Autre", IdTypeUser = 1, IdStatus = 1 };
 
-            var result = _controller.Put(userId, updatedUser);
+            var result = _controller.Put(userId, updatedUser, 1);
 
             Assert.IsInstanceOf<OkResult>(result);
         }
@@ -49,7 +49,7 @@ namespace Ressources.Back.Api.Tests
         {
             var userId = 1;
 
-            var result = _controller.Delete(userId);
+            var result = _controller.Delete(userId, 1);
 
             Assert.IsInstanceOf<OkResult>(result);
         }
@@ -57,7 +57,7 @@ namespace Ressources.Back.Api.Tests
         [Test]
         public void Authenticate_WithValidCredentials()
         {
-            var user = new UserModel { Id = 1, Login = "user1", Mdp = "password", Activate = 1, Age = 30, SituationFamiliale = "Célibataire", CSP = "Salarié", Loisir = "Football", Autre = "Autre", IdTypeUser = 1, IdStatus = 1 };
+            var user = new UserModel { Id = 1, Login = "user1", Mdp = "password", Activate = 1, Age = "31", SituationFamiliale = "Célibataire", CSP = "Salarié", Loisir = "Football", Autre = "Autre", IdTypeUser = 1, IdStatus = 1 };
             _mockUserRepository.Setup(repo => repo.Authenticate("user1", "password")).Returns(user);
 
             var result = _controller.Authenticate(new UserModel { Login = "user1", Mdp = "password" });
@@ -85,7 +85,7 @@ namespace Ressources.Back.Api.Tests
         public void GetById()
         {
             var userId = 1;
-            var expectedUser = new UserModel { Id = userId, Login = "TestUser", Mdp = "password", Activate = 1, Age = 25, SituationFamiliale = "Célibataire", CSP = "Étudiant", Loisir = "Musique", Autre = "Rien", IdTypeUser = 1, IdStatus = 1 };
+            var expectedUser = new UserModel { Id = userId, Login = "TestUser", Mdp = "password", Activate = 1, Age = "25", SituationFamiliale = "Célibataire", CSP = "Étudiant", Loisir = "Musique", Autre = "Rien", IdTypeUser = 1, IdStatus = 1 };
             _mockUserRepository.Setup(repo => repo.GetUserById(userId)).Returns(expectedUser);
 
             var result = _controller.GetUserById(userId);
@@ -100,8 +100,8 @@ namespace Ressources.Back.Api.Tests
         {
             var expectedUsers = new List<UserModel>
             {
-                new UserModel { Id = 1, Login = "User1", Mdp = "password", Activate = 1, Age = 25, SituationFamiliale = "Célibataire", CSP = "Étudiant", Loisir = "Musique", Autre = "Rien", IdTypeUser = 1, IdStatus = 1 },
-                new UserModel { Id = 2, Login = "User2", Mdp = "password", Activate = 1, Age = 30, SituationFamiliale = "Marié", CSP = "Salarié", Loisir = "Sport", Autre = "Rien", IdTypeUser = 2, IdStatus = 1 }
+                new UserModel { Id = 1, Login = "User1", Mdp = "password", Activate = 1, Age = "25", SituationFamiliale = "Célibataire", CSP = "Étudiant", Loisir = "Musique", Autre = "Rien", IdTypeUser = 1, IdStatus = 1 },
+                new UserModel { Id = 2, Login = "User2", Mdp = "password", Activate = 1, Age = "30", SituationFamiliale = "Marié", CSP = "Salarié", Loisir = "Sport", Autre = "Rien", IdTypeUser = 2, IdStatus = 1 }
             };
             _mockUserRepository.Setup(repo => repo.Read()).Returns(expectedUsers);
 
